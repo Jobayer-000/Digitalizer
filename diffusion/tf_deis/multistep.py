b0554368c4e8797f2dea564ebe3da6e6c308c32f
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-08-26 10:28:30 +0600

    Update multistep.py

--- diffusion/tf_deis/multistep.py
+++ diffusion/tf_deis/multistep.py
@@ -61,11 +61,8 @@ def get_coef_per_step_fn(sde, highest_order, order):
 def get_ab_eps_coef_order0(sde, highest_order, timesteps):
     _worker = get_coef_per_step_fn(sde, highest_order, 0)
     col_idx = tf.range(len(timesteps)-1)[:,None]
-    print(col_idx)
     idx = col_idx + tf.range(1)[None, :]
-    print(idx)
-    print(timesteps)
-    vec_ts_poly = timesteps[idx]
+    vec_ts_poly = tf.gather(timesteps, idx)
     return tf.map_fn(
         _worker,
    (timesteps[:-1], timesteps[1:], vec_ts_poly, 10000))
