13733d491bea03f2afc7abfbb0f9bbc58df4e097
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-08-26 23:29:53 +0600

    Update multistep.py

--- diffusion/tf_deis/multistep.py
+++ diffusion/tf_deis/multistep.py
@@ -45,7 +45,7 @@ def get_one_coef_per_step_fn(sde):
         integrand, t_inter, dt = _eps_coef_worker_fn(t_start, t_end, num_item)
         print(1)
         print(len(t_inter), ts_poly)
-        poly_coef = tf.map_fn(single_poly_coef, (t_inter, tf.ones((len(t_inter)))*ts_poly, tf.ones((len(t_inter)),tf.int32)*coef_idx))
+        poly_coef = tf.map_fn(single_poly_coef, (t_inter, tf.ones((len(t_inter)))*[ts_poly], tf.ones((len(t_inter)),tf.int32)*coef_idx))
         print(3)
         return tf.reduce_sum(integrand * poly_coef) * dt
     return _worker
