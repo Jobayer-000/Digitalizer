2dea424e425827c99f1ead32f9c292dbfb90ce9c
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-08-26 11:04:01 +0600

    Update multistep.py

--- diffusion/tf_deis/multistep.py
+++ diffusion/tf_deis/multistep.py
@@ -37,7 +37,9 @@ def get_one_coef_per_step_fn(sde):
         C_{ij}
         j: coef_idx
         """
-        t_start, t_end, ts_poly, coef_idx, num_item = x
+        coef_idx=0,
+        num_item=10000
+        t_start, t_end, ts_poly = x
         print(0)
         integrand, t_inter, dt = _eps_coef_worker_fn(t_start, t_end, num_item)
         print(1)
@@ -53,7 +55,8 @@ def get_coef_per_step_fn(sde, highest_order, order):
         C_i
         #!: we do flip of j here!
         """
-        t_start, t_end, ts_poly, num_item = x
+        num_item = 10000
+        t_start, t_end, ts_poly= x
         print(ts_poly)
         rtn = tf.zeros((highest_order+1, ), dtype=float)
         print(rtn)
@@ -73,7 +76,7 @@ def get_ab_eps_coef_order0(sde, highest_order, timesteps):
     
     return tf.map_fn(
         _worker,
-   (timesteps[:-1], timesteps[1:], vec_ts_poly, 10000))
+   (timesteps[:-1], timesteps[1:], vec_ts_poly))
 
 def get_ab_eps_coef(sde, highest_order, timesteps, order):
     assert isinstance(sde, MultiStepSDE)
@@ -91,7 +94,7 @@ def get_ab_eps_coef(sde, highest_order, timesteps, order):
     
     cur_coef = tf.map_fn(
         cur_coef_worker,
-        (timesteps[order:-1], timesteps[order+1:], vec_ts_poly, 10000)) #[3, 4, (0,1,2,3)]
+        (timesteps[order:-1], timesteps[order+1:], vec_ts_poly)) #[3, 4, (0,1,2,3)]
 
     return tf.concat(
         [
