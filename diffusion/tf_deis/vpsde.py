fbb48aa60e64af2958259ebe0a632012f7a73ea8
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-01 21:39:00 +0600

    Update vpsde.py

--- diffusion/tf_deis/vpsde.py
+++ diffusion/tf_deis/vpsde.py
@@ -89,9 +89,11 @@ def get_interp_fn(xp_, fp):
       print('xp',xp_.shape)
       if len(x.shape)>len(xp_.shape):
             xp = tf.tile(xp_[None,...], [x.shape[0], 1])
+            i = tf.clip_by_value(tf.searchsorted(xp, x, side='right'), clip_value_min=1, clip_value_max=len(xp) - 1)
+            xp = xp[0]
       else:
         xp = xp_
-      i = tf.clip_by_value(tf.searchsorted(xp, x, side='right'), clip_value_min=1, clip_value_max=len(xp) - 1)
+        i = tf.clip_by_value(tf.searchsorted(xp, x, side='right'), clip_value_min=1, clip_value_max=len(xp) - 1)
       df = tf.gather(fp,i) - tf.gather(fp, i - 1)
       dx = tf.gather(xp, i) - tf.gather(xp, i - 1)
       delta = x - tf.gather(xp, i - 1)
