a972498fd86d69dfb30d804021ded0cfe01ec7f3
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-01 16:15:04 +0600

    Update multistep.py

--- diffusion/tf_deis/multistep.py
+++ diffusion/tf_deis/multistep.py
@@ -4,8 +4,8 @@ from sde import MultiStepSDE
 
 def get_integrator_basis_fn(sde):
     def _worker(t_start, t_end, num_item):
-        dt = (t_end - t_start) / num_item
-        print('dt',dt)
+        dt = tf.cast((t_end - t_start) / num_item,tf.float32)
+      
         t_inter = tf.transpose(tf.linspace(t_start, t_end, num_item), [1,0])
         psi_coef = sde.psi(t_inter, t_end[...,None])
         integrand = sde.eps_integrand(t_inter)
