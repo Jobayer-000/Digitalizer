d3e91f2efd857ddbf6a54353009ee24380032137
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-01 15:56:58 +0600

    Update vpsde.py

--- diffusion/tf_deis/vpsde.py
+++ diffusion/tf_deis/vpsde.py
@@ -85,9 +85,10 @@ def get_interp_fn(xp, fp):
           raise ValueError("xp and fp must be one-dimensional arrays of equal size")
       
       print('x', x)
-      print('fp', fp)
+      print('xp',xp)
       i = tf.clip_by_value(tf.searchsorted(xp, x, side='right'), clip_value_min=1, clip_value_max=len(xp) - 1)
       print('i', i)
+      print('fp', fp)
       df = tf.gather(fp,i) - tf.gather(fp, i - 1)
       dx = tf.gather(xp, i) - tf.gather(xp, i - 1)
       delta = x - tf.gather(xp, i - 1)
