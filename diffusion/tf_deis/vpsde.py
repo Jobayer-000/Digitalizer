e81bdf33428d5c114bdcf97441a299f1f6cbc428
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-03 00:01:29 +0600

    Update vpsde.py

--- diffusion/tf_deis/vpsde.py
+++ diffusion/tf_deis/vpsde.py
@@ -62,7 +62,7 @@ class VPSDE(ExpSDE, MultiStepSDE):
     def eps_integrand(self, vec_t):
         with tf.GradientTape() as tape:
             tape.watch(vec_t)
-            y = self.t2alpha_fn(vec_t)
+            y = tf.math.log(self.t2alpha_fn(vec_t))
         d_log_alpha_dtau = tape.gradient(y, vec_t)
         print('d_log_alpha_dtau', d_log_alpha_dtau)
         integrand = -0.5 * d_log_alpha_dtau / tf.sqrt(1 - self.t2alpha_fn(vec_t))
