6dfc71f851e5db979edb4f640a22a3a532b550b7
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-02 12:53:57 +0600

    Update vpsde.py

--- diffusion/tf_deis/vpsde.py
+++ diffusion/tf_deis/vpsde.py
@@ -100,11 +100,15 @@ def get_interp_fn(xp_, fp):
       else:
         xp = xp_
         i = tf.clip_by_value(tf.searchsorted(xp, x, side='right'), clip_value_min=1, clip_value_max=len(xp) - 1)
+      print('xp', xp)
       df = tf.gather(fp,i) - tf.gather(fp, i - 1)
+      print('df', df)
       dx = tf.gather(xp, i) - tf.gather(xp, i - 1)
       delta = x - tf.gather(xp, i - 1)
+      print('delta', delta)
       f = tf.where((dx == 0), tf.gather(fp, i), tf.gather(fp, i - 1) + (delta / dx) * df)
-      return f
+      print('fd', tf.where((dx == 0), tf.gather(fp, i), tf.gather(fp, i - 1) + (delta / dx))
+      return f 
   return _fn
 
 class DiscreteVPSDE(VPSDE):
